a2d287178eeb2b4bf4abee1ff98052f227499145
Aditi Joshi <aditi6301@gmail.com>
2021-05-12 10:30:54 +0530

    Initial

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
+const app = express()
+
+let sessionOptions = session({
+  secret: "JavaScript is sooooooooo coool",
+  store: MongoStore.create({client: require('./db')}),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+})
+
+app.use(sessionOptions)
+app.use(flash())
+
+app.use(function(req, res, next) {
+  // make all error and success flash messages available from all templates
+  res.locals.errors = req.flash("errors")
+  res.locals.success = req.flash("success")
+
+  // make current user id available on the req object
+  if (req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
+  
+  // make user session data available from within view templates
+  res.locals.user = req.session.user
+  next()
+})
+
+const router = require('./router')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.use('/', router)
+
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server)
+io.use(function(socket, next) {
+  sessionOptions(socket.request, socket.request.res, next)
+})
+
+io.on('connection', function(socket) {
+  if (socket.request.session.user) {
+    let user = socket.request.session.user
+
+  
+
+    socket.emit('welcome', {username: user.username, avatar: user.avatar})
+    socket.on('chatMessageFromBrowser', function(data) {
+     socket.broadcast.emit('chatMessageFromServer', {message: data.message, username: user.username, avatar: user.avatar})
+  
+    })
+}
+ 
+})
+
+module.exports = server
